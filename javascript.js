36962d0800b85fe835e86792acb3298e3e9ed758
sharifullah shirzad <sharifullahshirzad81@gmail.com>
2022-08-30 11:37:03 +0430

    my file

---
+++ javascript.js
@@ -0,0 +1,107 @@
+
+
+//var x = 12;
+//var y= 13;
+//var z = 12;
+//document.write(x==y);
+//document.write( x==x);
+
+//  Array
+
+//const fourts =['apple','orange','shalgham','gazry']
+              //  1           2       3           4
+        //Array name 
+       //document.write(fourts[2]);
+
+ //const parson ={lname:'khan',aname:'jan',bname:'gulajan',cname:'sidajan',dname:'gulmarjam'};
+ //document.write(parson.lname);
+
+
+ //const car ={name:'BMW' , mode:2000 , color:'black' , engine:'v8'}
+ //for (let key in car){
+ //   document.write(car[key])
+ //}
+
+
+
+
+
+
+
+
+
+// function bootdelivery(p1){
+   // alert('take the mony 209 afghani gite dodi ');
+//alert('move strigh');
+//alert('turn ;left');
+//alert('move');
+//alert('move');
+//alert('move');
+//alert('turn ;lright');
+//alert('turn ;left');
+//alert('give  the dodi  to nanwaiy');
+//alert('give 20 dodi');
+//alert('come back');
+//alert('come back');
+//alert('come back');
+//alert('come back');
+//alert('give me the reemaining 9 afghani ');
+
+
+
+//}
+
+ //bootdelivery("give dodi ");
+
+ //var x =209;
+ //var y=20;
+ //var z=9;
+ //document.write(z)
+
+
+//  var breadprice;
+//  var mybread;
+//  function goGetbread(mybadge,mybread){
+//      mybread = math.floor(mybadge%breadprice);
+//      mybread = math.floor(mybadge/breadprice);
+
+//      document.write("number of bread:"+mybread);
+//      document.write("my change money :"+mychangr); 
+// }
+// goGetbread(333 ,20);
+
+
+// function myname(name){
+//     return "your name is :" +name;
+    
+// }
+// document.write( )
+
+// vra 
+
+// function temprature(celsius){
+
+// }
+
+// var temp;
+// function temprature(a)
+// {
+//     temp = Math.floor(5/9*a-32)
+//     document.write("celsius :" +temp)
+// }
+ 
+// temprature(88)
+
+// function toCelsius(far)
+// {
+//     var cel= ((5/9) * (far-32) );
+//     return cel;
+// }
+// document.write(toCelsius(96.6));
+
+function toCelsius(far){
+
+    return ((far-32)*5)/9 ;
+}
+
+document.getElementById("demo").innerHTML="to day temp is " + toCelsius(56) + 'ce' ;
